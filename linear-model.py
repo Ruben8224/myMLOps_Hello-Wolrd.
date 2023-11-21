5c6666fe4164361e4cc8b6e6608764971e35f1a9
Ruben8224 <Tartas_casr@hotmail.com>
2023-11-20 21:27:28 -0600

    master

--- linear-model.py
+++ linear-model.py
@@ -23,17 +23,17 @@ linear_model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(units=1, input_shape=[1], name='Single')
 ])
 
-# Compilar el modelo
+
 linear_model.compile(optimizer=tf.keras.optimizers.SGD(), loss=tf.keras.losses.mean_squared_error)
 print(linear_model.summary())
 
-# Entrenar el modelo
+
 linear_model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=20)
 
-# Realizar predicciones
+
 predictions = linear_model.predict([[0.0], [2.0], [3.1], [4.2], [5.2]]).tolist()
 print(predictions)
 
-# Guardar el modelo
-export_path = 'linear-model/1/'  # Cambiar el número del modelo si guardas múltiples versiones
+
+export_path = 'linear-model/1/'
 tf.saved_model.save(linear_model, os.path.join('./', export_path))
